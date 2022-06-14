d6c90897705a7076366f98f0f56bc8e0fe6069d4
Daria Panchenko <user576717@gmail.com>
2022-06-14 10:04:04 +0100

    my HW-16

---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+function getFibonacci(num) {
+
+    function* getNumberFibonacci() {
+        let a;
+        let b;
+        let current = a = b = 1;
+
+        yield 1;
+
+        while (true) {
+            current = b;
+            yield current;
+            b = a + b;
+            a = current;
+        }
+    }
+
+    let fibonacci = getNumberFibonacci();
+    let result;
+    for (let i = 0; i < num; i++) {
+        result = fibonacci.next().value;
+    }
+    return result;
+
+}
+
+
+function cacheEl(fn) {
+
+    const cash = new Map();
+
+    return function () {
+
+        const arg = Array.from(arguments)[0];
+
+        if (cash.has(arg)) {
+            return cash.get(arg);
+        }
+
+        const result = fn.call(this, arg);
+
+        cash.set(arg, result);
+
+        return result;
+
+    }
+
+};
+
+
+let callNumber = cacheEl(getFibonacci);
+console.log(callNumber(7));
+console.log(callNumber(7));
+console.log(callNumber(8));
+console.log(callNumber(11));
+
+
+
+
+
+
+
+
+
+
+
+
